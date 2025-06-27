671a2d34ef323b319d3cece1750da6304b66d3e4
Kaushal16~Hustler <pro.kaushalkr16@gmail.com>
2025-06-27 13:09:46 +0530

    your commit

---
+++ ArrayBasics.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int A[10] = {4, 6, 8, 9};
+	int i;
+	// cout<<A[3]<<endl;
+	for (i = 0; i < 10; i++)
+
+	{
+		cout << A[i] << endl;
+	}
+
+	return 0;
+}
